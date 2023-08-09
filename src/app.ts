dafb113b9cdd45777e519cebd1e00a3096fce571
Ikedinobi Onyeka <ikedinobicruz7@gmail.com>
2023-08-09 15:44:22 +0100

    logging all available routes

--- src/app.ts
+++ src/app.ts
@@ -2,27 +2,29 @@ import cors from 'cors';
 import "express-async-errors"
 import express, { Application, Request } from "express";
 import { PORT } from './config';
-import { IRoute } from './interfaces/route.interface';
+import { Route } from './interfaces/route.interface';
 import morganMiddleware from './middlewares/morgan.middleware';
 import { logger } from './utils/logger';
 import ErrorMiddleWare from './middlewares/error.middleware';
+import expressListRoutes from "express-list-routes"
 
 export default class App {
 
     public app: Application;
     public port: string | number;
 
-    constructor(routes: IRoute[]) {
+    constructor(routes: Route[]) {
         this.app = express();
         this.port = PORT || 8000;
         this.initializeMiddlewares()
         this.initializeRoutes(routes)
         this.initializeErrorHandling()
+        this.listRoutes()
     }
 
     public listen(): void {
         this.app.listen(this.port, () => {
-            logger.info(`⚡️[server]: Server is running @ http://localhost:${this.port}`)
+            logger.info(`📡 [server]: Server is running @ http://localhost:${this.port}`)
         })
     }
 
@@ -33,7 +35,7 @@ export default class App {
         this.app.use(morganMiddleware)
     }
 
-    private initializeRoutes(routes: IRoute[]): void {
+    private initializeRoutes(routes: Route[]): void {
         routes.forEach(route => {
             this.app.use("/api/v1", route.router)
         })
@@ -41,6 +43,15 @@ export default class App {
 
     private initializeErrorHandling() {
         this.app.use(ErrorMiddleWare.handleErrors)
+    }
+
+    private listRoutes() {
+        expressListRoutes(
+            this.app,
+            {
+                logger: ((method, space, path) => logger.info(`🚏 [Routes]: ${method}  ${path}`))
+            }
+        )
     }
 
 }
